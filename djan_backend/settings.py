4e215fdf5dc95183e6f05cb6b2069e0d4b8a65f3
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-09-07 19:37:56 -0300

    preparando para railway

--- djan_backend/settings.py
+++ djan_backend/settings.py
@@ -133,5 +133,6 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://client-todo-rho.vercel.app/"
  ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
